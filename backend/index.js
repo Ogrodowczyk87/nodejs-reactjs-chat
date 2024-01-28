6fe23130bc621ec55dfc3e883bb9ee29a901d201
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2024-01-28 16:43:28 +0000

    edit files

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,17 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username, secret: "sha256..." });
+    // Get or create user on Chat Engine!
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "Private-Key": "edd81549-90be-4993-8772-13cf4b87b121" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
-app.listen(3002);
+app.listen(3001);
